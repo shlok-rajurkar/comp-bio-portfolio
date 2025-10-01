b3e516d167566fc0053250e0d3cbb010d68ea1c1
Dragonappendix <228423547+Dragonappendix@users.noreply.github.com>
2025-09-30 22:26:28 -0700

    Add code to construct NA percent dataframe

--- metabolomics_project/scripts/quantify_NA.r
+++ metabolomics_project/scripts/quantify_NA.r
@@ -1,33 +1,41 @@
 # Takes in a dataframe and returns the percentage of NA values in each column
 
-required_packages <- c("dplyr", "tools", "tidyverse")
+required_packages <- c("dplyr", "tools")
 
 for (package in required_packages){
   if (!(package %in% installed.packages()[, 1])) {
     install.packages(package)
   }
-  library(package)
+  library(package, character.only = TRUE)
 }
 
 file_paths_entered <- commandArgs(trailingOnly = TRUE)
 
 for (file_path in file_paths_entered) {
-  quantify_NA(file_path)
+  print(file_path)
+  print(quantify_NA(file_path))
 }
 
 quantify_na <- function(file_path) {
-read_fn <- switch(file_ext(file_path), "xpt" = read_xpt, "csv" = read_csv)
-dataframe <- read_fn(file_path)
-if (!(class(dataframe) %in% c("data.frame", "tibble"))) {
-  stop("Provide a dataframe.")
-}
+  read_fn <- {
+    switch(tools::file_ext(file_path)
+           , "xpt" = haven::read_xpt, "csv" = readr::read_csv)
+  }
+  dataframe <- read_fn(file_path)
+  if (!(class(dataframe) %in% c("data.frame", "tibble"))) {
+    stop("Provide a dataframe.")
+  }
 
-# na_means <- 
+  na_percents <- c()
 
-# for (column in ncol(dataframe) {
+  for (column in seq_len(ncol(dataframe))) {
+    na_percent <- sum(is.na(dataframe[[column]])) / length(dataframe[[column]])
+    na_percents <- c(na_percents, na_percent)
+  }
 
-# })
+  column_names <- colnames(dataframe)
 
-#outMatrix <- matrix(data = dataVec, nrow = nrow, ncol = ncol, byrow = byrow)
+  na_frame <- data.frame("Column" = column_names, "NA Percents" = na_percents)
 
+  na_frame
 }
