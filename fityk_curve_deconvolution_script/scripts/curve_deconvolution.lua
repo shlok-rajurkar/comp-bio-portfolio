9ba8fcdcc365c9a99b200d163fe542deea22bcbc
Shlok Rajurkar <228423547+shlok-rajurkar@users.noreply.github.com>
2025-10-23 13:27:47 -0700

    Add array for IM bins

--- fityk_curve_deconvolution_script/scripts/curve_deconvolution.lua
+++ fityk_curve_deconvolution_script/scripts/curve_deconvolution.lua
@@ -1,1 +1,5 @@
 -- Placeholder file for potentially more complex workflows
+
+local bins = {0, 7.65, 10.5, 14.5, 18, 19, 19.9,
+        20.49, 20.82, 21.41, 22, 22.46, 
+        23.33, 25, 29.6, 33.5, 42.4, 54.7, 100}
