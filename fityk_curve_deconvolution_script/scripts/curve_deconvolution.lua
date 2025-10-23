d82ae47903a786c6f43e5cde4fee00a602608d2b
Shlok Rajurkar <228423547+shlok-rajurkar@users.noreply.github.com>
2025-10-23 15:53:49 -0700

    Add function to place single Gaussian

--- fityk_curve_deconvolution_script/scripts/curve_deconvolution.lua
+++ fityk_curve_deconvolution_script/scripts/curve_deconvolution.lua
@@ -6,3 +6,12 @@
 local bins = {0, 7.65, 10.5, 14.5, 18, 19, 19.9,
         20.49, 20.82, 21.41, 22, 22.46, 
         23.33, 25, 29.6, 33.5, 42.4, 54.7, 100}
+
+-- function to place single gaussian based on start and end values
+
+function placeSingleGaussian(start, stop)
+    F:execute("@*: guess Gaussian [" .. start .. ":" .. stop .. "]")
+    print(start)
+    print(stop)
+end
+
