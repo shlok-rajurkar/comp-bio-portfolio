2f040efe719ec091d850424f57e6ef7fa1fa5b15
Shlok Rajurkar <228423547+shlok-rajurkar@users.noreply.github.com>
2025-10-23 16:07:24 -0700

    Correct precision of last bin value

--- fityk_curve_deconvolution_script/scripts/curve_deconvolution.lua
+++ fityk_curve_deconvolution_script/scripts/curve_deconvolution.lua
@@ -5,7 +5,8 @@
 
 local bins = {0, 7.65, 10.5, 14.5, 18, 19, 19.9, 
         20.49, 20.82, 21.41, 22, 22.46, 
-        23.33, 25, 29.6, 33.5, 42.4, 54.7, 100}
+        23.33, 25, 29.6, 33.5, 42.4, 52.29136658
+}
 
 -- Function to place single gaussian based on start and end values
 
