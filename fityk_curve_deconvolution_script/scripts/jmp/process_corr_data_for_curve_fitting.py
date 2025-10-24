356e51d77ab02ebd742e8f22f61a80a21140ea28
Shlok Rajurkar <228423547+shlok-rajurkar@users.noreply.github.com>
2025-10-24 16:14:10 -0700

    Add fn to load jmp file into pandas dataframe

--- fityk_curve_deconvolution_script/scripts/jmp/process_corr_data_for_curve_fitting.py
+++ fityk_curve_deconvolution_script/scripts/jmp/process_corr_data_for_curve_fitting.py
@@ -1,2 +1,22 @@
 import jmp
+import numpy
+import pandas as pd
+
+# Load current jmp table as df
+
+df = jmp.current()
+
+# Load df into pandas dataframe
+
+def jmp_to_pandas(jmp_table):
+    pandasdf = pd.DataFrame()
+    for col in jmp_table[:len(jmp_table)]:
+        pandasdf[col.name] = list(col)
+    return pandasdf
+
+jmp_to_pandas(df)
+
+
+
+
 
