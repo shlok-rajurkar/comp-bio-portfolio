1d83a64cb758ca05c069bf110e255fbf3c096312
Shlok Rajurkar <228423547+shlok-rajurkar@users.noreply.github.com>
2025-10-24 16:47:30 -0700

    Add direction for future work and commend out assert statement for now

--- fityk_curve_deconvolution_script/scripts/jmp/process_corr_data_for_curve_fitting.py
+++ fityk_curve_deconvolution_script/scripts/jmp/process_corr_data_for_curve_fitting.py
@@ -1,6 +1,11 @@
 # Replaces diagonal of ones along corr matrix with avg of closest values
 
 import jmp
+import jmputils
+
+jmputils.jpip('install --upgrade', 'pip setuptools')
+jmputils.jpip('install', 'numpy pandas')
+
 import numpy
 import pandas as pd
 
@@ -20,7 +25,14 @@ dataframe = jmp_to_pandas(dt)
 
 # Ensure table is square
 
-assert len(dataframe.columns) == len(dataframe.rows), "Correlation matrix must be square"
+print(dataframe)
+
+assert len(dataframe.columns) == len(dataframe[0]), "Correlation matrix must be square"
+
+# Column processing functions
+# Replace [0] in first column with [1]
+# Replace [i] in middle columns with np.mean([i-1], [i+1])
+# Replace [len.column] in last column with 
 
 
 
