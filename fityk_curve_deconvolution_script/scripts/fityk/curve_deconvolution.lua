2a734c6d6d12b36655e91b4898625be70e882116
Shlok Rajurkar <228423547+shlok-rajurkar@users.noreply.github.com>
2025-10-30 16:55:17 -0700

    Add new bins var for manual fitting

--- fityk_curve_deconvolution_script/scripts/fityk/curve_deconvolution.lua
+++ fityk_curve_deconvolution_script/scripts/fityk/curve_deconvolution.lua
@@ -1,11 +1,16 @@
 -- Divides a single spectrum into Gaussian curves based on bins,
--- which can be edited below. By default, uses 
+-- which can be edited below. By default, uses IM bins
 
 -- Define bins based on IM parameters
 
-local bins = {0, 7.65, 10.5, 14.5, 18, 19, 19.9, 
+local standardIMBins = {
+    0, 7.65, 10.5, 14.5, 18, 19, 19.9, 
         20.49, 20.82, 21.41, 22, 22.46, 
         23.33, 25, 29.6, 33.5, 42.4, 52.29136658
+}
+
+local mzCorrBins = {
+
 }
 
 -- Function to place single gaussian based on start and end values
@@ -26,4 +31,4 @@ function binGaussian(bins)
     end
 end
 
-binGaussian(bins)
+binGaussian()
